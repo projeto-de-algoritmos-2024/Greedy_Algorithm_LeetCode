699f4e01b2e7c0fbaefa131f3ca6c94ba7fa3545
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-07-29 15:35:44 -0300

    Versão final do exercício. Feito correções para completar os casos de teste.

--- course_schedule_III/course_schedule_III.py
+++ course_schedule_III/course_schedule_III.py
@@ -3,12 +3,12 @@ from typing import List
 
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
-        max_heap = []  
-        courses.sort()  
-
+        max_heap = [] 
+        courses.sort(key=lambda x: x[1])  
         total_time = 0
+
         for duration, end_time in courses:
-            heapq.heappush(max_heap, duration)  
+            heapq.heappush(max_heap, -duration)  
             total_time += duration  
 
             if total_time > end_time:
